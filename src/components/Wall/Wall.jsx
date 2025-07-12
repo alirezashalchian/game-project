e85ff02d21c337930945a5554789be0d5d03e2c5
alirezashalchian <144257851+alirezashalchian@users.noreply.github.com>
2025-07-12 17:09:34 +0330

    Create Wall.jsx

---
+++ src/components/Wall/Wall.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { RigidBody, CuboidCollider } from "@react-three/rapier";
+import { roomConfig } from "../Room/roomConfig";
+import { WallPiece } from "./WallPiece";
+
+export const Wall = ({ position, rotation, type, hasDoor = true, roomId }) => {
+  const halfInnerSize = roomConfig.innerSize / 2;
+  const sidePieceWidth = (roomConfig.innerSize - roomConfig.doorSize) / 2;
+
+  return (
+    <group position={position} rotation={rotation}>
+      <RigidBody
+        type="fixed"
+        userData={{ isWall: true, wallType: type, roomId }}
+      >
+        {hasDoor ? (
+          // Wall with door opening (for walls that should have doors)
+          <>
+            {/* Top piece */}
+            <WallPiece
+              position={[0, halfInnerSize - sidePieceWidth / 2, 0]}
+              size={[
+                roomConfig.innerSize,
+                sidePieceWidth,
+                roomConfig.wallThickness,
+              ]}
+              type={type}
+            />
+
+            {/* Bottom piece */}
+            <WallPiece
+              position={[0, -halfInnerSize + sidePieceWidth / 2, 0]}
+              size={[
+                roomConfig.innerSize,
+                sidePieceWidth,
+                roomConfig.wallThickness,
+              ]}
+              type={type}
+            />
+
+            {/* Left piece */}
+            <WallPiece
+              position={[-halfInnerSize + sidePieceWidth / 2, 0, 0]}
+              size={[
+                sidePieceWidth,
+                roomConfig.doorSize,
+                roomConfig.wallThickness,
+              ]}
+              type={type}
+            />
+
+            {/* Right piece */}
+            <WallPiece
+              position={[halfInnerSize - sidePieceWidth / 2, 0, 0]}
+              size={[
+                sidePieceWidth,
+                roomConfig.doorSize,
+                roomConfig.wallThickness,
+              ]}
+              type={type}
+            />
+          </>
+        ) : (
+          // Solid wall (no door) - for phantom-adjacent walls
+          <WallPiece
+            position={[0, 0, 0]}
+            size={[
+              roomConfig.innerSize,
+              roomConfig.innerSize,
+              roomConfig.wallThickness,
+            ]}
+            type={type}
+          />
+        )}
+      </RigidBody>
+    </group>
+  );
+};
