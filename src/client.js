5684b6b2d5e7164c0b1153fee4c8d92143be98df
alirezashalchian <144257851+alirezashalchian@users.noreply.github.com>
2025-08-10 21:58:32 +0330

    created: client.js to connect to the server

---
+++ src/client.js
@@ -0,0 +1,23 @@
+import { Client, getStateCallbacks } from 'colyseus.js';
+ 
+async function connect() {
+    const client = new Client('http://localhost:2567');
+    const room = await client.joinOrCreate('my_room', {
+        /* custom join options */
+    });
+    const $ = getStateCallbacks(room);
+ 
+    // Listen to 'player' instance additions
+    $(room.state).players.onAdd((player, sessionId) => {
+        console.log('Player joined:', player, sessionId);
+    });
+ 
+    // Listen to 'player' instance removals
+    $(room.state).players.onRemove((player, sessionId) => {
+        console.log('Player left:', player, sessionId);
+    });
+ 
+    return room;
+}
+ 
+connect();
