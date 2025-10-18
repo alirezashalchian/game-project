8b6c4a8fce96918e483e21509018f506226bd663
alirezashalchian <alirezashalchian1995@gmail.com>
2025-10-18 23:26:58 +0330

    fix: boost performance

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -7,7 +7,7 @@ import { CharacterProvider } from "./CharacterContext";
 import { SaveSystem } from "./SaveSystem";
 import { ModelSelectorUI } from "./ModelSelectorUI";
 import { GravityChangeUI } from "./GravityChangeUI";
-import RoomWithPhysics from "./RoomWithPhysics";
+import RoomWithPhysics from "./Rooms";
 import GameLoadingPage from "./LoadingPage";
 import { useGameLoading } from "../hooks/useGameLoading";
 
@@ -64,11 +64,20 @@ export default function GamePage() {
         >
           <ModelSelectorUI />
           <Canvas
-            shadows
+            shadows={false}
             camera={{ fov: 75 }}
             style={{ position: "fixed", top: 0, left: 0 }}
+            gl={{
+              antialias: false,
+              powerPreference: "high-performance",
+              stencil: false,
+              depth: true,
+            }}
+            dpr={[1, 1.5]}
+            performance={{ min: 0.5 }}
+            frameloop="always"
           >
-            <Physics gravity={[0, 0, 0]}>
+            <Physics gravity={[0, 0, 0]} timeStep="vary">
               <RoomWithPhysics />
             </Physics>
           </Canvas>
