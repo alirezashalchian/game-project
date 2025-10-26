d09722ef87bdfafa744de590cc3f7ca4e1bd6e70
alirezashalchian <alirezashalchian1995@gmail.com>
2025-10-26 18:55:10 +0330

    feat: add playerId type

--- server/src/rooms/schema/MyRoomState.ts
+++ server/src/rooms/schema/MyRoomState.ts
@@ -2,6 +2,7 @@ import { Schema, type, MapSchema } from "@colyseus/schema";
 
 export class Player extends Schema {
   @type("string") sessionId: string;
+  @type("string") playerId: string = "";
   @type("number") x: number = 0;
   @type("number") y: number = 0;
   @type("number") z: number = 0;
